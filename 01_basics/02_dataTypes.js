ba1aef0554b057bf09545727de26a9928dd3aa8e
Swastik061522 <swastiktimilsina@gmail.com>
2024-09-03 20:10:51 +0000

    add datatypes file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+//datatypes in javascript
+"use strict";  // When written this, a newer version of javascript is used
+
+let age = 20      // number type
+
+let name = "Swastik"   // String type
+
+let isStudent = true    // boolean type
+
+let hasJob;   // undefined
+
+let salary = null   // object type
+
+console.log(typeof hasJob)   // To check the datatypes
+
+
+/*
+Some other types are 
+1. bigint
+2. symbol ==> unique
+*/
