27eadb2338390176e58d2c20cdf146934b269157
Swastik061522 <swastiktimilsina@gmail.com>
2024-09-03 21:10:41 +0000

    add typeconversion

---
+++ 01_basics/03_dataTypeConversion.js
@@ -0,0 +1,40 @@
+//Converting from number to a string
+/*
+let age = 20
+let stringAge = String(age)
+console.log(stringAge)
+console.log (typeof(stringAge))
+*/
+
+// Converting from string to a number
+/*
+let stringAge = "20"
+let numberAge = Number(stringAge)
+console.log(numberAge)
+console.log(typeof(numberAge))
+
+let anotherAge = "20sbdofn"
+let numberAge = Number(anotherAge)
+console.log(numberAge);
+console.log(typeof(numberAge))    // Gives NaN which is not a number but still it is of number type
+*/
+
+// Converting from boolean to number
+
+/*
+let isboolean = false
+let number = Number(isboolean)
+console.log(number);
+console.log(typeof(number))
+*/
+
+// Converting from number to boolean
+
+// let number = 0
+// let isboolean = Boolean(number)
+// console.log(isboolean)
+// Any number other than 0 will give true in boolean
+
+
+
+
