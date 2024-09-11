cfed9cb53e3f6c2da81ba99fb730407c817f830b
Swastik061522 <swastiktimilsina@gmail.com>
2024-09-11 17:32:52 +0000

    add basics part 2 including array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,2 +1,28 @@
 // arrays
+ 
+const array = [1,2,3,4,5]
+
+//Another way to initialize an array
+const newArray = new Array(1,2,3,4,5)
+
+// But when using this method, keep in mind that only one argument is passed on Array() c0nstructor,
+// it doesnot create an array containing one element. Instead, it will create an empty array 
+// having length of the value passed in constructor
+
+// console.log(array);
+// console.log(newArray);
+
+// array methods
+// array.push(6)
+// array.pop()
+// const slicedArray = array.slice(2,4)  -> Does not change original array
+// console.log(slicedArray);
+// array.splice(1, 3)   -> Change original array
+
+// const joined = array.join("\n")  -> returns an array in string seperated by provided string("\n") in this case
+console.log(array);
+
+
+
+
 
